6a71fdc43d76cc0c57a7fcaa94cd37c11ef3ef86
Abhishek Kovuri <36876451+abhishekkovuri@users.noreply.github.com>
2025-09-15 23:39:21 +0530

    Time: 0 ms (100.00%) | Memory: 54 MB (26.80%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -5,13 +5,24 @@
 var lengthOfLastWord = function (s) {
     let n = s.length - 1
 
-    while (s[n] === ' ') {
-        n--;
-    }
+    // 2 loops
+    // while (s[n] === ' ') {
+    //     n--;
+    // }
+    // let count = 0;
+    // while (n >= 0 && s[n] !== ' ') {
+    //     count++;
+    //     n--;
+    // }
 
-    let count = 0;
-    while (n >= 0 && s[n] !== ' ') {
-        count++;
+    // 1 loop 
+    let count = 0
+    while(n>=0) {
+        if(s[n] !== ' ') {
+            count++
+        } else if(count > 0) {
+            break;
+        }
         n--;
     }
     return count;
