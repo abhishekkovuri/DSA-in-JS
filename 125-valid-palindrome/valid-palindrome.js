3d96865ef164dd60a8b4666d475775c41ea62bd4
Abhishek Kovuri <36876451+abhishekkovuri@users.noreply.github.com>
2025-09-16 23:52:01 +0530

    Time: 7 ms (39.41%) | Memory: 61.8 MB (16.75%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -3,13 +3,27 @@
  * @return {boolean}
  */
 var isPalindrome = function (s) {
+    // s = s.toLowerCase()
+    // let filteredString = reverseString = ''
+    // for (let i = 0; i < s.length; i++) {
+    //     if (s[i].match(/[a-z0-9]/i)) {
+    //         filteredString += s[i]
+    //         reverseString = s[i] + reverseString;
+    //     }
+    // }
+    // return reverseString === filteredString;
+
     s = s.toLowerCase()
-    let filteredString = reverseString = ''
-    for (let i = 0; i < s.length; i++) {
-        if (s[i].match(/[a-z0-9]/i)) {
-            filteredString += s[i]
-            reverseString = s[i] + reverseString;
+    let x = 0, y = s.length - 1
+
+    while(x < y) {
+        if(!s[x].match(/[a-z0-9]/i)) x++;
+        else if(!s[y].match(/[a-z0-9]/i)) y--;
+        else if(s[x] !== s[y]) return false;
+        else {
+            x++;
+            y--;
         }
     }
-    return reverseString === filteredString;
+    return true;
 };
