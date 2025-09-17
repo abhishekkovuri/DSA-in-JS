063225313f76ecc31a7ab8713ab137b1fd5f23dc
Abhishek Kovuri <36876451+abhishekkovuri@users.noreply.github.com>
2025-09-17 21:45:23 +0530

    Time: 2 ms (80.87%) | Memory: 64.3 MB (5.58%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (h) {
+    let i = 0, j = h.length - 1;
+    let maxArea = 0;
+    while (i < j) {
+        if (h[i] > h[j]) {
+            maxArea = Math.max(maxArea, (j - i) * h[j])
+            j--;
+        } else {
+            maxArea = Math.max(maxArea, (j - i) * h[i])
+            i++;
+        }
+    }
+    return maxArea;
+};
