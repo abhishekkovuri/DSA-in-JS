3be7555ab8551fdbe3ba88e51161b0f031383c3c
Abhishek Kovuri <36876451+abhishekkovuri@users.noreply.github.com>
2025-09-18 22:49:59 +0530

    Time: 6 ms (68.80%) | Memory: 56.2 MB (28.19%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    const hashMapST = {}
+    const hashMapTS = {}
+    for(let i = 0; i < t.length; i++) {
+        if(!hashMapST[s[i]] && !hashMapTS[t[i]]) {
+            hashMapST[s[i]] = t[i]
+            hashMapTS[t[i]] = s[i]
+        } else if(hashMapTS[t[i]] !== s[i]) {
+            return false;
+        } else if(hashMapST[s[i]] !== t[i]) {
+            return false;
+        }
+    }
+    return true;
+};
