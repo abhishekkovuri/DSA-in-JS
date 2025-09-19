2c7f59ec27d3a0626d0a661674e36aa4aa34cadc
Abhishek Kovuri <36876451+abhishekkovuri@users.noreply.github.com>
2025-09-20 00:32:58 +0530

    Time: 0 ms (100.00%) | Memory: 54.3 MB (15.56%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.js
+++ 225-implement-stack-using-queues/implement-stack-using-queues.js
@@ -1,7 +1,7 @@
 
 var MyStack = function () {
     this.q1 = []
-    this.q2 = []
+    // this.q2 = []
 };
 
 /** 
@@ -18,12 +18,13 @@ MyStack.prototype.push = function (x) {
 MyStack.prototype.pop = function () {
     let n = this.q1.length
     for (let i = 0; i < n - 1; i++) {
-        this.q2.push(this.q1.shift())
+        // this.q2.push(this.q1.shift())
+        this.q1.push(this.q1.shift())
     }
     let ans = this.q1.shift()
-    let temp = this.q1
-    this.q1 = this.q2
-    this.q2 = temp
+    // let temp = this.q1
+    // this.q1 = this.q2
+    // this.q2 = temp
 
     return ans;
 };
@@ -34,13 +35,15 @@ MyStack.prototype.pop = function () {
 MyStack.prototype.top = function () {
     let n = this.q1.length;
     for (let i = 0; i < n - 1; i++) {
-        this.q2.push(this.q1.shift());
+        // this.q2.push(this.q1.shift());
+        this.q1.push(this.q1.shift());
     }
     let front = this.q1[0];
-    this.q2.push(this.q1.shift());
-    let temp = this.q1;
-    this.q1 = this.q2;
-    this.q2 = temp;
+    // this.q2.push(this.q1.shift());
+    this.q1.push(this.q1.shift());
+    // let temp = this.q1;
+    // this.q1 = this.q2;
+    // this.q2 = temp;
     return front;
 };
 
