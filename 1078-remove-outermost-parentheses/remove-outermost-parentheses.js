18802773f29ec35e73bc22fb733808d08d9f4e9f
Abhishek Kovuri <36876451+abhishekkovuri@users.noreply.github.com>
2025-09-20 21:20:52 +0530

    Time: 3 ms (79.43%) | Memory: 55.9 MB (70.89%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.js
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.js
@@ -3,21 +3,30 @@
  * @return {string}
  */
 var removeOuterParentheses = function (s) {
-    let stack = []
+    // let stack = []
+    let level = 0;
     let output = ''
     for (let i = 0; i < s.length; i++) {
+        // with stack
+        // if (s[i] === '(') {
+        //     stack.push(s[i])
+        //     let len = stack.length
+        //     if (len > 1) {
+        //         output += s[i]
+        //     }
+        // } else {
+        //     let len = stack.length
+        //     if (len > 1) {
+        //         output += s[i]
+        //     }
+        //     stack.pop()
+        // }
         if (s[i] === '(') {
-            stack.push(s[i])
-            let len = stack.length
-            if (len > 1) {
-                output += s[i]
-            }
+            level++
+            output += (level > 1) ? s[i] : ''
         } else {
-            let len = stack.length
-            if (len > 1) {
-                output += s[i]
-            }
-            stack.pop()
+            output += (level > 1) ? s[i] : ''
+            level--;
         }
     }
     return output;
