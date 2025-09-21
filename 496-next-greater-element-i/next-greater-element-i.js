bb737e7a7e9aa900bf32829cd27f17abcda86cbb
Abhishek Kovuri <36876451+abhishekkovuri@users.noreply.github.com>
2025-09-21 23:39:10 +0530

    Time: 4 ms (52.85%) | Memory: 58.2 MB (21.41%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.js
+++ 496-next-greater-element-i/next-greater-element-i.js
@@ -11,28 +11,16 @@ var nextGreaterElement = function (n1, n2) {
     map[n2[n - 1]] = -1
 
     for (let i = n - 2; i >= 0; i--) {
-        let top = s[s.length - 1]
-        if (top > n2[i]) {
-            map[n2[i]] = top
-        } else {
-            while (s.length) {
-                if (s[s.length - 1] < n2[i]) {
-                    s.pop();
-                } else {
-                    map[n2[i]] = s[s.length - 1]
-                    break;
-                }
-            }
-            if (!s.length) {
-                map[n2[i]] = -1
+        while (s.length) {
+            if (s[s.length - 1] < n2[i]) {
+                s.pop();
+            } else {
+                map[n2[i]] = s[s.length - 1]
+                break;
             }
         }
+        if (!s.length) map[n2[i]] = -1
         s.push(n2[i])
     }
-    console.log(map)
-    const output = []
-    for (let i = 0; i < n1.length; i++) {
-        output.push(map[n1[i]])
-    }
-    return output;
+    return n1.map(x => map[x]);
 };
