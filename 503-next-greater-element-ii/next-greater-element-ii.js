fa4169b051f0c86418e60b110a6a721b6f5fde30
Abhishek Kovuri <36876451+abhishekkovuri@users.noreply.github.com>
2025-09-22 11:58:33 +0530

    Time: 8 ms (51.32%) | Memory: 64.3 MB (10.46%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var nextGreaterElements = function (a) {
+    const na = [...a, ...a]
+    const n = na.length
+    const ans = Array(n).fill(-1)
+    const s = [na[n - 1]]
+
+    for (let i = n - 2; i >= 0; i--) {
+        while (s.length) {
+            const top = s[s.length - 1]
+            if (na[i] < top) {
+                ans[i] = top
+                break;
+            } else {
+                s.pop()
+            }
+        }
+        s.push(na[i])
+    }
+
+    return ans.slice(0, n/2)
+
+};
