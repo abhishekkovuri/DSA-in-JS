48366d0966a97d86515b377cfba8dc8156a0d2f3
Abhishek Kovuri <36876451+abhishekkovuri@users.noreply.github.com>
2025-09-22 11:58:52 +0530

    Time: 7 ms (61.78%) | Memory: 61.7 MB (60.36%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.js
+++ 503-next-greater-element-ii/next-greater-element-ii.js
@@ -3,24 +3,42 @@
  * @return {number[]}
  */
 var nextGreaterElements = function (a) {
-    const na = [...a, ...a]
-    const n = na.length
-    const ans = Array(n).fill(-1)
-    const s = [na[n - 1]]
+    // const na = [...a, ...a]
+    // const n = na.length
+    // const ans = Array(n).fill(-1)
+    // const s = [na[n - 1]]
 
-    for (let i = n - 2; i >= 0; i--) {
+    // for (let i = n - 2; i >= 0; i--) {
+    //     while (s.length) {
+    //         const top = s[s.length - 1]
+    //         if (na[i] < top) {
+    //             ans[i] = top
+    //             break;
+    //         } else {
+    //             s.pop()
+    //         }
+    //     }
+    //     s.push(na[i])
+    // }
+
+    // return ans.slice(0, n/2)
+
+    const n = a.length
+    const ans = Array(n).fill(-1)
+    const s = [a[n - 1]]
+
+    for (let i = (2 * n) - 2; i >= 0; i--) {
         while (s.length) {
             const top = s[s.length - 1]
-            if (na[i] < top) {
-                ans[i] = top
+            if (a[i % n] < top) {
+                ans[i % n] = top
                 break;
             } else {
                 s.pop()
             }
         }
-        s.push(na[i])
+        s.push(a[i % n])
     }
 
-    return ans.slice(0, n/2)
-
+    return ans.slice(0, n)
 };
