0bfc96d61c94bd40f2964939d5a93dcbdcd40cb9
Abhishek Kovuri <36876451+abhishekkovuri@users.noreply.github.com>
2025-09-28 20:35:10 +0530

    Time: 1 ms (87.97%) | Memory: 57.6 MB (13.58%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -3,14 +3,13 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    const hashMap = {}
-    for(let i = 0; i < nums.length; i++) {
-        const diff = target - nums[i];
-        if(hashMap.hasOwnProperty(diff)) {
-          return [hashMap[diff], i]
+var twoSum = function (nums, target) {
+    const sumSet = {}
+    for (let i = 0; i < nums.length; i++) {
+        let diff = target - nums[i]
+        if (sumSet.hasOwnProperty(diff)) {
+            return [i, sumSet[diff]]
         }
-        hashMap[nums[i]] = i
+        sumSet[nums[i]] = i;
     }
-    return [];
 };
