71af492860201dae58951e464f800c68ae0d27f1
Abhishek Kovuri <36876451+abhishekkovuri@users.noreply.github.com>
2025-09-28 22:43:11 +0530

    Time: 3 ms (20.41%) | Memory: 56.4 MB (5.21%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    let l = 0;
+    let r = 0;
+
+    while(l !== s.length && r !== t.length) {
+        if(s[l] === t[r]) {
+            r++;
+            l++;
+        } else {
+            r++;
+        }
+    }
+    return l === s.length;
+};
