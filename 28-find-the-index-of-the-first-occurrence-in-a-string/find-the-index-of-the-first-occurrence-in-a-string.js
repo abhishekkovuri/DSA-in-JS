68d7b44e7705efc8989b58e9aef76d1b116a33d7
Abhishek Kovuri <36876451+abhishekkovuri@users.noreply.github.com>
2025-09-28 23:01:39 +0530

    Time: 0 ms (100.00%) | Memory: 54.5 MB (26.34%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(h, n) {
+    return h.indexOf(n);
+    // let l = 0;
+    // let r = 0;
+
+    // while(l !== h.length && r !== n.length) {
+    //     if(n[r] === h[l]) {
+    //         r++
+    //         l++
+    //     } else {
+
+    //     }
+    // }
+    // return -1;
+
+
+};
