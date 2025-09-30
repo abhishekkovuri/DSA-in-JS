52344690f069215fb44f49fb20e7e776b7311992
Abhishek Kovuri <36876451+abhishekkovuri@users.noreply.github.com>
2025-09-30 23:33:14 +0530

    Time: 0 ms (100.00%) | Memory: 54.6 MB (24.08%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -3,21 +3,58 @@
  * @param {string} needle
  * @return {number}
  */
-var strStr = function(h, n) {
+var strStr = function(haystack, needle) {
     // return h.indexOf(n);
     
     // sliding window
-    let hl = h.length;
-    let nl = n.length;
+    // let hl = h.length;
+    // let nl = n.length;
 
-    for(let i = 0; i <= hl - nl; i++) {
-        let j = 0;
-        for(j = 0; j < nl; j++) {
-            if(h[i + j] !== n[j]) {
-                break;
+    // for(let i = 0; i <= hl - nl; i++) {
+    //     let j = 0;
+    //     for(j = 0; j < nl; j++) {
+    //         if(h[i + j] !== n[j]) {
+    //             break;
+    //         }
+    //     }
+    //     if(j === nl) return i;
+    // }
+
+    // KMP
+    let n = haystack.length;
+    let m = needle.length;
+    let lps = [0];
+    let i = 0;
+    let j = 1;
+    while(j < m){
+        if(needle[i] === needle[j]) {
+            lps[j] = i+1;
+            ++i;
+            ++j;
+        } else {
+            if(i == 0) {
+                lps[j] = 0;
+                ++j;
+            } else{
+                i = lps[i-1];
             }
         }
-        if(j === nl) return i;
+    }
+    i = j = 0;
+    while(i < n) {
+        if(haystack[i] === needle[j]) {
+            ++i;
+            ++j;
+        } else{
+            if(j==0) {
+                ++i;
+            } else {
+                j = lps[j-1];
+            }
+        }
+        if(j === m){
+            return i - m;
+        }
     }
     return -1;
 };
