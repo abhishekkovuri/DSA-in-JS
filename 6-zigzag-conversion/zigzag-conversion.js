52e81cb3c60aacfbb3a5ff4330f8ae8829c7a547
Abhishek Kovuri <36876451+abhishekkovuri@users.noreply.github.com>
2025-10-14 20:14:41 +0530

    Time: 3 ms (91.36%) | Memory: 58.5 MB (61.28%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function (s, numRows) {
+    if (numRows === 1) return s;
+    const rows = new Array(Math.min(numRows, s.length)).fill("");
+    let currRow = 0;
+    let goingDown = false;
+
+    for (let c of s) {
+        rows[currRow] += c
+        if (currRow === 0 || currRow === numRows - 1) goingDown = !goingDown
+        currRow += goingDown ? 1 : -1
+    }
+
+    return rows.join('')
+};
