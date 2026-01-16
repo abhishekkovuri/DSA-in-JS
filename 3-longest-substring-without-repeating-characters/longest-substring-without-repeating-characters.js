44650bf55fc5400321a56b4e8bba9816fd8c2bd5
Abhishek Kovuri <36876451+abhishekkovuri@users.noreply.github.com>
2026-01-16 08:30:48 +0530

    Time: 6 ms (68.52%) | Memory: 56.2 MB (87.77%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -3,20 +3,32 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-    const charMap = {};
-    let l = 0;
-    let mL = 0;
+    // const charMap = {};
+    // let l = 0;
+    // let mL = 0;
 
-    for (let r = 0; r < s.length; r++) {
-        const currentChar = s[r];
+    // for (let r = 0; r < s.length; r++) {
+    //     const currentChar = s[r];
 
-        if (currentChar in charMap && charMap[currentChar] >= l) {
-            l = charMap[currentChar] + 1;
+    //     if (currentChar in charMap && charMap[currentChar] >= l) {
+    //         l = charMap[currentChar] + 1;
+    //     }
+
+    //     charMap[currentChar] = r;
+    //     mL = Math.max(mL, r - l + 1);
+    // }
+
+    // return mL;
+    let set = new Set();
+    let left = 0, max = 0;
+
+    for(let right = 0; right < s.length; right++) {
+        while(set.has(s[right])) {
+            set.delete(s[left++])
         }
-
-        charMap[currentChar] = r;
-        mL = Math.max(mL, r - l + 1);
+        set.add(s[right]);
+        max = Math.max(max, right - left + 1)
     }
 
-    return mL;
+    return max;
 };
