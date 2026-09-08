10f1d8d71f755a38a6befa8b5e464aa8b9f7e646
Abhishek Kovuri <36876451+abhishekkovuri@users.noreply.github.com>
2026-09-08 23:11:05 +0530

    Time: 24 ms (80.56%) | Memory: 62.3 MB (48.69%) - LeetSync

--- 455-assign-cookies/assign-cookies.js
+++ 455-assign-cookies/assign-cookies.js
@@ -4,15 +4,13 @@
  * @return {number}
  */
 var findContentChildren = function (g, s) {
-    g.sort((a,b) => a-b);
-    s.sort((a,b) => a-b);
+    g.sort((a, b) => a - b)
+    s.sort((a, b) => a - b)
 
     let child = 0;
-    let i = 0;
-    let j = 0;
-
-    while(i <= g.length && j <= s.length) {
-        if(s[j] >= g[i]) {
+    let i = j = 0;
+    while (i < g.length && j < s.length) {
+        if (s[j] >= g[i]) {
             child++;
             i++;
         }
